f7aafcb1ca5164679ceaa95261139434be138abd
aiglesFoudroyants <1302743@bdeb.qc.ca>
2015-09-08 08:55:25 -0400

    Ajout du UI de login et de register

--- Epyks/WinRegister.xaml.cs
+++ Epyks/WinRegister.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,19 +8,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Epyks
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Logique d'interaction pour WinRegister.xaml
+	/// </summary>
+	public partial class WinRegister : Window
+	{
+		public WinRegister()
+		{
+			this.InitializeComponent();
+			
+			// Insérez le code requis pour la création d’objet sous ce point.
+		}
+	}
 }
