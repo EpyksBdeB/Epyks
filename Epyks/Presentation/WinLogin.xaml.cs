bc7f4eae354967a8e090350510d7ac0725de1d71
aiglesFoudroyants <1302743@bdeb.qc.ca>
2015-09-08 13:03:30 -0400

    Refactor du Ui et modification du UI register

---
+++ Epyks/Presentation/WinLogin.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Epyks.Presentation
+{
+    /// <summary>
+    /// Logique d'interaction pour WinLogin.xaml
+    /// </summary>
+    public partial class WinLogin : Window
+    {
+        public WinLogin()
+        {
+            InitializeComponent();
+
+        }
+
+        private void TxtPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TxtPassword.Background.Opacity = 0;
+        }
+
+        private void TxtPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TxtPassword.Password.Length == 0)
+            {
+                TxtPassword.Background.Opacity = 1;
+            }
+        }
+    }
+}
---
+++ Epyks/Presentation/WinRegister.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Epyks.Presentation
+{
+	/// <summary>
+	/// Logique d'interaction pour WinRegister.xaml
+	/// </summary>
+	public partial class WinRegister : Window
+	{
+		public WinRegister()
+		{
+			this.InitializeComponent();
+			
+			// Insérez le code requis pour la création d’objet sous ce point.
+		}
+
+        private void PasswordGotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = (PasswordBox) sender;
+            passwordBox.Background.Opacity = 0;
+        }
+
+        private void PasswordLostFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = (PasswordBox)sender;
+            if (passwordBox.Password.Length == 0)
+            {
+                passwordBox.Background.Opacity = 1;
+            }
+        }
+	}
+}
