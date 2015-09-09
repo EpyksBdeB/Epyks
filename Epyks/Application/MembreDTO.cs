3b4d4ed3a056a06b261807865913a061b125856a
Christelle Melissa <adeolamelissasissoko95@gmail.com>
2015-09-09 10:15:24 -0400

    Ajout classe MembreDTO

---
+++ Epyks/Application/MembreDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epyks.Application
+{
+    public class MembreDTO
+    {
+        private String name { get; set; }
+        private String surname { get; set; }
+        private String username { get; set; }
+        private String password { get; set; }
+        private String email { get; set; }
+        private char gender { get; set; }
+
+        public MembreDTO()
+        {
+        }
+    }
+}
