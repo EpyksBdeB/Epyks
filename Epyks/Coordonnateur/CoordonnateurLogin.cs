7033bf94ae029a4c40a9aedddeba916ad6f6f259
castropeo <>
2015-09-09 09:33:31 -0700

    Ajout du Dossier Coordonnateur

---
+++ Epyks/Coordonnateur/CoordonnateurLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epyks.Coordonnateur
+{
+    private enum StatusLogin
+    {
+        INVALID_PASSWORD,
+        INVALID_USERNAME
+    }
+
+    private enum StatusRegister
+    {
+        EXISTING_USERNAME
+    }
+
+    class CoordonnateurLogin
+    {
+
+        public CoordonnateurLogin()
+        {
+        }
+
+        private void Login()
+        {
+
+        }
+
+        private void Register()
+        {
+
+        }
+    }
+
+}
