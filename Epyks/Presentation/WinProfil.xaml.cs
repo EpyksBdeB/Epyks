81b5aa8bfa7c3e423bc98c5c64d761fd84aef44d
castropeo <>
2015-09-08 22:30:19 -0700

    Ajout du UI Profil

---
+++ Epyks/Presentation/WinProfil.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Epyks
+{
+	/// <summary>
+	/// Logique d'interaction pour WinProfil.xaml
+	/// </summary>
+	public partial class WinProfil : Window
+	{
+		public WinProfil()
+		{
+			this.InitializeComponent();
+			
+			// Insérez le code requis pour la création d’objet sous ce point.
+		}
+	}
+}
