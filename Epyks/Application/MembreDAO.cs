a06a3cc2d3cc46a1e574df284b2a61e6fc6a19af
Christelle Melissa <adeolamelissasissoko95@gmail.com>
2015-09-11 14:15:24 -0400

    Début d'implémentation de la BD dans la classe "Membre DAO"

--- Epyks/Application/MembreDAO.cs
+++ Epyks/Application/MembreDAO.cs
@@ -1,26 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using MySql;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Windows;
 
 namespace Epyks.Application
 {
     public class MembreDAO : MembreDAOInterface
     {
         // Probleme avec l'implementation de la BD donc pour le moment je travaille avec une liste
-        List<Membre> listofMembers;
+       // List<Membre> listofMembers;
+        private MySql.Data.MySqlClient.MySqlConnection connection = null;
+        private string myConnectionString = null;
+     
 
         public MembreDAO()
         {
-            listofMembers = new List<Membre>();
+            initializeDatabase();
+            /** listofMembers = new List<Membre>();
             Membre member1 = new Membre("Robert", "Gallagher", "Gally", "root", "gally@hotmail.fr", 'M');
             Membre member2 = new Membre("Robert", "Gallagher", "Gally", "root", "gally@hotmail.fr", 'M');
             listofMembers.Add(member1);
-            listofMembers.Add(member2);	
+            listofMembers.Add(member2);	 **/
         }
 
+        public void initializeDatabase()
+        {
+            myConnectionString = "server=localhost;uid=melissa_07;" + "pwd=Cartigan0;database=test;";
+            try
+            {
+                connection = new MySql.Data.MySqlClient.MySqlConnection();
+                connection.ConnectionString = myConnectionString;
+                connection.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                // message erreur
+                MessageBox.Show(ex.Message);
+
+            }
+        }
 
         public List<Membre> getAllMembers()
         {
