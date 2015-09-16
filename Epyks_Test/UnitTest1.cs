78272083493623d87eb665b86f567c9268d0e87e
castropeo <>
2015-09-16 09:02:12 -0400

    Ajout du projet Test, + un test (Login)

---
+++ Epyks_Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using Epyks.Coordonnateur;
+
+namespace Epyks_Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        
+        [Test]
+        public void TestLogin()
+        {
+            CoordonnateurLogin coord = new CoordonnateurLogin();
+            Assert.IsTrue(coord.Login("Olivier Castro", "gr007,,"));          
+        }
+    }
+}
