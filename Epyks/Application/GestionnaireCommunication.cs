b50e8e1733ce54d6f208408db334c6999b389177
Gabiel Lamarre <1302743@bdeb.qc.ca>
2015-11-25 10:09:06 -0500

    modification du serveur

--- Epyks/Application/GestionnaireCommunication.cs
+++ Epyks/Application/GestionnaireCommunication.cs
@@ -14,7 +14,7 @@ namespace Epyks.Application
     internal class GestionnaireCommunication
     {
         private const int PORT = 8181;
-        private const string HOSTNAME = "aegaur.ddns.net";
+        private const string HOSTNAME = "localhost";
 
         private TcpClient tcpClient;
         private StreamReader reader;
@@ -24,13 +24,13 @@ namespace Epyks.Application
 
         public GestionnaireCommunication(Membre membreCourant)
         {
-            ////this.membreCourant = membreCourant;
-            ////isReading = true;
-            ////tcpClient = new TcpClient(HOSTNAME, PORT);
-            ////reader = new StreamReader(tcpClient.GetStream());
-            ////writer = new StreamWriter(tcpClient.GetStream());
-            ////writer.AutoFlush = true;
-            ////writer.WriteLine(Convert.ToString(membreCourant.id));
+            this.membreCourant = membreCourant;
+            isReading = true;
+            tcpClient = new TcpClient(HOSTNAME, PORT);
+            reader = new StreamReader(tcpClient.GetStream());
+            writer = new StreamWriter(tcpClient.GetStream());
+            writer.AutoFlush = true;
+            writer.WriteLine(Convert.ToString(membreCourant.id));
         }
 
         public void EcrireMessage(Message message)
